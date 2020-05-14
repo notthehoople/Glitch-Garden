d485794e9eb0e4f31c12e76416034cb476b67c67
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-14 08:33:59 +0100

    Loading screen to Start Menu created

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] float loadScreenDelay = 3f;
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTimeThenLoadNextScene());
+        }
+    }
+
+    IEnumerator WaitForTimeThenLoadNextScene()
+    {
+        yield return new WaitForSeconds(loadScreenDelay);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Glitch Garden
+
+Working through Glitch Garden section of [gamedev.tv's Unity 2D course](https://gamedev.tv/courses)
+Built in Unity 2018.4.22f1
+Uses assets from [Glitch The Game](https://www.glitchthegame.com)
+
+## Instructions
+
+### Running the code
+
+* Clone the repo
+* Open Unity
+* Play the game!
+
+## TODO
+
+### Feature Areas
+*
+
+### General
+* 
+* 
+
+### Specific
+*
+
+
