fa1ca14f90b96bab5d756ff4d5405c4fdffc269e
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-17 14:38:12 +0100

    Fix movement during jump and walk animations
    If jump has a movement speed then the jump happens and the lizard then slides along the ground. Using Animation Events, set the movement speed to 0 during jump, then 1 at the start of walk.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     // Configuration Parameters
-    [Range (0f, 5f)] [SerializeField] float walkSpeed = 1f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // [Range (0f, 5f)]
+    float currentSpeed = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
 }
