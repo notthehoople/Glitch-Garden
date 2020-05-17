8dd6c3eaae1acb52707ac4eacac511484077ac7d
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-17 14:24:17 +0100

    Added random enemy spawn on all lanes
    Prefab everything except EventSystem

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
