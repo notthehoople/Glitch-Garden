84485564a037bcdbd982bf9948df16acafebdde0
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-20 08:36:09 +0100

    Added explosion when attacker dies
    Includes a hack to the x position (under "Shape") where the VFX instantiates

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,14 +6,24 @@ public class Health : MonoBehaviour
 {
     // Configuration Parameters
     [SerializeField] int health = 100;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
 
     public void DealDamage(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX) { return; }
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplosion);
     }
 
     public int GetHealth()
