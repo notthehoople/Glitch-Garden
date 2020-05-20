b374b9d4a21fb63249382bcfd9b693357a701dbd
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-20 08:51:06 +0100

    Spawn defender on mouse click
    Added Core Game Area to accept mouse clicks
    Added DefenderSpawner script to deal with mouse clicks and spawn the defender

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    // Configruation Parameters
+    [SerializeField] GameObject defender;
+
+    public void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+    }
+}
