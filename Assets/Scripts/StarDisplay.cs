1638dcc22e2459835c92903cd51dda2ebb1d79c3
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-22 11:10:27 +0100

    Start of Resource System
    Added Text to the Selection UI to display resources
    Added methods to StarDisplay.cs for updating the resources
    TODO: bring the resource text display in front of the Selection UI so it is brighter

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] int stars = 100;
+
+    // Cached References
+    Text starText;
+
+    void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int starsToAdd)
+    {
+        stars += starsToAdd;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int starsToSpend)
+    {
+        if (stars >= starsToSpend)
+        {
+            stars -= starsToSpend;
+            UpdateDisplay();
+
+        }
+    }
+}
