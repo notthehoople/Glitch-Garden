d38ecdae91948396aa00964dbd809dd39f4ca4ed
Mark Bradshaw <notthehoople@googlemail.com>
2020-06-03 08:37:36 +0100

    Detect if an Attacker is in the same lane as a defender
    Added code to Shooter.cs

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,6 +5,50 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject gun, projectile;
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Shoot pew pew");
+            // TODO Change animation state to shooting
+        }
+        else
+        {
+            Debug.Log("Sit and wait");
+            // TODO Change animation state to idle
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        // Look for any attackers in the same lane as us
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        return true;
+    }
 
     public void Fire()
     {
