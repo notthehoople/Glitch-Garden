b7b690d1af63776160fa12f24a9ebdbdd3e84c41
Mark Bradshaw <notthehoople@googlemail.com>
2020-06-05 17:45:52 +0100

    Add Fox attacker
    - add new attacker with animations
    - add a Fox specific script to trigger jumping over the gravestone

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        // If the thing we bump into has a Defender component then it's a defender we should attack
+        if (otherObject.GetComponent<Gravestone>())
+        {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
