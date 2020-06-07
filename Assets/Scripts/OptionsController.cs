630da2866b0296721dd6e56a5d0ef343c07b2dfe
Mark Bradshaw <notthehoople@googlemail.com>
2020-06-07 13:49:40 +0100

    Made Volume button change the volume of the music playing
    - added MusicPlayer object to Options Screen (TODO: Delete!)
    - added MusicPlayer and music to the Splash screen
    - added OptionsController to control the volume based on volume slider

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.5f;
+
+    // Cached References
+    MusicPlayer musicPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicPlayer = FindObjectOfType<MusicPlayer>();
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player found...did you start from splash screen?");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        // Save the volume level before we go back to the Main Menu
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
